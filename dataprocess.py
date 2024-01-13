ee7e23d5f528d4da3a9343cf59eefa6f0f2921cd
Your Name <you@example.com>
2024-01-13 21:33:07 +1200

    adding 3 csv files to read and display dataframe

--- dataprocess.py
+++ dataprocess.py
@@ -6,5 +6,5 @@ file_path = r'C:\Users\Iele\Desktop\Quantium_task1\daily_sales_data_0.csv'
 # Load data into a DataFrame
 df = pd.read_csv(file_path, header=0)
 
-# Display the DataFrame
+#display the data frame
 print(df)
