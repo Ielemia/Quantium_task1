1efca953e42ba6fb17ce816642e2a96a85693391
Your Name <you@example.com>
2024-01-14 19:48:23 +1200

    change visualizer to visualise

--- dash_visualisation.py
+++ dash_visualisation.py
@@ -16,7 +16,7 @@ app = dash.Dash(__name__)
 
 # Define layout of the app
 app.layout = html.Div(children=[
-    html.H1(children='Pink Morsel Sales Visualizer'),
+    html.H1(children='Pink Morsel Sales Visualise'),
 
     dcc.Graph(
         id='sales-bar-chart',
